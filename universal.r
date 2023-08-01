a4bf83a17a49696cdf4b70e0984fecf301900451
André <88557686+ausdrehBio@users.noreply.github.com>
2023-08-01 16:07:35 +0200

    Update universal.r

--- universal.r
+++ universal.r
@@ -52,7 +52,9 @@ library(gprofiler2)
 
 ############# read in arguments #############
 args <- commandArgs(trailingOnly = TRUE)
-print("\n\nteeeeeeeesttttt\n\n")
+print("\n\n")
+print("teeeeeeeesttttt")
+print("\n\n")
 if (length(args) < 3) {
 
     stop("Usage: Rscript universal.r savepath path2data path2meta algorithms(optional: 'base', 'extra' or 'all') min_size(optional) protein_mapping(optional) timepoint(optional)", call.=FALSE)
